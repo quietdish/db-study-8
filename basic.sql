f2ab4a18e5f313f4ac976d770250b20474204ddd
quietdish <whale61@naver.com>
2026-07-06 15:28:08 +0900

    DB 시작

---
+++ basic.sql
@@ -0,0 +1,29 @@
+--sql query 작성 영역
+-- 한줄 주석
+/*
+여러줄
+주석
+처리
+*/
+
+DB(DataBase) 데이터가 저장되는 공간(개념)
+DBMS(Database Management System) 데이터베이스 관리 시스템
+
+PC -> Windows OS Oracle DBMS <- 접속 도구/툴(sqlplus, sqldeveloper, DBeaver, Orange, etc)로 접속 후 사용
+                            (SQL 쿼리문을 사용해서 DB 데이터를 사용/조작)
+                            
+
+Oracle DB 서버에 접속
+* 접속 계정
+id : scott
+pw : tiger
+
+SQL (쿼리문)
+Structured Query Language
+
+DB 사용/조작 -> 데이터 조각 명령 (쿼리문을 작성해서...)
+CRUD
+Create  생성
+Read    조회(읽기)
+Update  수정
+Delete  삭제
